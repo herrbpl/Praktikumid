2af95d5a3bbb3951d0e064410ed45145ebfb6aaf
Siim Aus <herrbpl@gmail.com>
2015-09-08 19:47:58 +0300

    Initial commit

---
+++ src/praktikum1/Loop.java
@@ -0,0 +1,11 @@
+package praktikum1;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		while (true) {
+			System.out.println("Too much XYZ");
+		}
+	}
+
+}
---
+++ src/praktikum1/Tere.java
@@ -0,0 +1,10 @@
+package praktikum1;
+
+
+
+
+public class Tere {
+    public static void main (String[] kasurida) {
+        System.out.println("Tere, Siim Aus!");
+    }
+}
